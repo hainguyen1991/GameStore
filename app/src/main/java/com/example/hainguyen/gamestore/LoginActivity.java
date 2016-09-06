f7aa8b6b70eafbcb6c35144e2a082c7446d92385
1608-V0041\Hai Nguyen <hainguyen.na.287@gmail.com>
2016-09-06 16:37:16 +0700

    4:37pm

--- app/src/main/java/com/example/hainguyen/gamestore/LoginActivity.java
+++ app/src/main/java/com/example/hainguyen/gamestore/LoginActivity.java
@@ -1,5 +1,7 @@
 package com.example.hainguyen.gamestore;
 
+import android.content.Intent;
+import android.os.Handler;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 
@@ -9,5 +11,19 @@ public class LoginActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_login);
+    }
+
+    @Override
+    protected void onStart() {
+        super.onStart();
+        new Handler().postDelayed(new Runnable() {
+            @Override
+            public void run() {
+                Intent it = new Intent(LoginActivity.this, MenuActivity.class);
+                startActivity(it);
+                finish();
+
+            }
+        },3000);
     }
 }
