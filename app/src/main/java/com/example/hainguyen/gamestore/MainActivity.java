70aa28c530af0a5b5db7884986c125d6ad6ac1e1
1608-V0041\Hai Nguyen <hainguyen.na.287@gmail.com>
2016-09-06 15:10:00 +0700

    upload project

---
+++ app/src/main/java/com/example/hainguyen/gamestore/MainActivity.java
@@ -0,0 +1,28 @@
+package com.example.hainguyen.gamestore;
+
+import android.content.Intent;
+import android.os.Handler;
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+
+public class MainActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+    }
+
+    @Override
+    protected void onStart() {
+        super.onStart();
+        new Handler().postDelayed(new Runnable() {
+            @Override
+            public void run() {
+                Intent i = new Intent(MainActivity.this, LoginActivity.class);
+                startActivity(i);
+                finish();
+            }
+        }, 3000);
+    }
+}
